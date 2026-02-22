d8bcf546bc7d0ef0a2eabde4f2fabe6098c835da
Huzaifa Bin Sohail <huzaifaq458@gmail.com>
2026-02-22 09:48:05 -0500

    new

--- Constructor Function/script.js
+++ Constructor Function/script.js
@@ -3,13 +3,14 @@ function Createpencil(name, qty, color, company){
   this.qty = qty,
   this.color = color,
   this.company = company
-  this.write = function(text){
+  Createpencil.prototype.write = function(text){
  let h1 = document.createElement("h1");
  h1.textContent = text;
  h1.style.color = this.color;
   document.body.appendChild(h1)}
 };
 
-let pencil1 = new Createpencil("huzi", 10, "black", "uiz")
+let pencil1 = new Createpencil("huzi", 10, "blue", "uiz")
 let pencil2 = new Createpencil("huzi", 10, "red", "uiz")
 
+pencil1.write("kaisya ho")
